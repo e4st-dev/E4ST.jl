baae85cac0abfcf8321a5d589572920f07faecf2
Ethan Russell <erussell@rff.org>
2022-09-27 15:43:53 -0400

    Add test outline

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,33 @@ using Test
 using E4ST
 
 @testset "Test E4ST" begin
+    @testset "Unit Tests" begin
+        @testset "Test Loading Config" begin
+            @test_skip "No Tests Written"
+        end
+        @testset "Test Saving Config" begin
+            @test_skip "No Tests Written"
+        end
+        @testset "Test Loading Data" begin
+            @test_skip "No Tests Written"
+        end
+        @testset "Test Initializing Data" begin
+            @test_skip "No Tests Written"
+        end
+        @testset "Test Setting Up Model" begin
+            @test_skip "No Tests Written"
+        end
+        @testset "Test Optimizing Model" begin
+            @test_skip "No Tests Written"
+        end
+        @testset "Test Parsing and Saving Results" begin
+            @test_skip "No Tests Written"
+        end
+        @testset "Test Iteration" begin
+            @test_skip "No Tests Written"
+        end
+    end
+    @testset "System Tests" begin
+        @test_skip "No Tests Written"
+    end
 end
