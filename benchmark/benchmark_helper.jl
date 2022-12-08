b808e3150e43b1339766388dc2a9d60638009fe7
Ethan Russell <erussell@rff.org>
2022-12-08 16:55:10 -0500

    Add benchmarks for load shaping

--- benchmark/benchmark_helper.jl
+++ benchmark/benchmark_helper.jl
@@ -4,44 +4,32 @@ using OrderedCollections
 using CSV
 using E4ST
 
-function make_random_inputs(;n_bus = 100, n_gen = 100, n_branch=100, n_af=100, n_hours=100, n_demand = 200, af_file=true)
+function make_random_inputs(;n_bus = 100, n_gen = 100, n_branch=100, n_af=100, n_hours=100, n_demand = 200, n_demand_shape=100, af_file=true, demand_shape_file=true)
     Random.seed!(1)
 
-    countries = ["narnia", "calormen", "archenland", "telmar"]
 
     ## Make Bus Table
     bus = DataFrame(
         ref_bus = fill(false, n_bus),
         pd = rand(n_bus),
-        country = rand(countries, n_bus)
+        country = rand(countries(), n_bus)
     )
     ref_bus_idx = rand(1:n_bus)
     bus.ref_bus[ref_bus_idx] = true
 
-    genfuels = ["biomass","nuclear","ng","coal","solar","hydro", "wind", "geothermal"]
-    gentypes = OrderedDict(
-        "biomass"=>["biomass","biomass_new"],
-        "nuclear"=>["nuclear","nuclear_new"],
-        "ng"=>["ng","ng_cc","ng_ccccs"],
-        "coal"=>["coal","coal_ccs"],
-        "solar"=>["solar","solar_new"],
-        "hydro"=>["hydro","hydro_new"],
-        "wind"=>["wind","wind_new"],
-        "geothermal"=>["hydrothermal","deepgeo_new"],
-    )
-
     gen = DataFrame(
         bus_idx = rand(1:n_bus, n_gen),
         status = trues(n_gen),
-        genfuel = rand(genfuels, n_gen),
+        genfuel = rand(genfuels(), n_gen),
         pcap_min = zeros(n_gen),
         pcap_max = ones(n_gen),
         vom = rand(n_gen),
         fom = rand(n_gen),
         capex = rand(n_gen),
     )
+    gt = gentypes()
     gen.gentype = map(gen.genfuel) do gf
-        rand(gentypes[gf])
+        rand(gt[gf])
     end
 
     branch = DataFrame(
@@ -65,59 +53,136 @@ function make_random_inputs(;n_bus = 100, n_gen = 100, n_branch=100, n_af=100, n
     CSV.write(joinpath(@__DIR__, "data/gen.csv"), gen)
     CSV.write(joinpath(@__DIR__, "data/branch.csv"), branch)
     CSV.write(joinpath(@__DIR__, "data/time.csv"), time)
-    years = ["y$y" for y in 2030:5:2050]
-    year_strs = vcat(years, "")
     config = OrderedDict(
         :out_path=>abspath(@__DIR__,"out"),
         :gen_file=>abspath(@__DIR__,"data/gen.csv"),
         :bus_file=>abspath(@__DIR__,"data/bus.csv"),
         :branch_file=>abspath(@__DIR__, "data/branch.csv"),
         :hours_file=>abspath(@__DIR__, "data/time.csv"),
-        :years=>years,
+        :years=>years(),
         :optimizer=>OrderedDict(
             :type=>"HiGHS",
         ),
         :mods=>Modification[]
     )
+
+    demand = rand_demand(;n_bus, n_demand)
+    CSV.write(joinpath(@__DIR__, "data/demand.csv"), demand)
+    config[:demand_file] = abspath(@__DIR__, "data/demand.csv")
+
     if af_file
-        af = DataFrame(
-            "area" => String[],
-            "subarea" => String[],
-            "genfuel" => String[],
-            "gentype" => String[],
-            "year"=>String[],
-            "joint" => Int64[],
-            "status" => Bool[],
-            ("h$n"=>Float64[] for n in 1:n_hours)...
-        )
-        joint = 1
-        while nrow(af) < n_af
-            for country in countries
-                joint += 1
-                for genfuel in genfuels, gentype in gentypes[genfuel]
-                    year = rand(year_strs)
-                    push!(af, ("country", country, genfuel, gentype, year, joint, true, rand(n_hours)...))
-                    if nrow(af) == n_af
-                        break
-                    end
-                end
+        af = rand_af(;n_hours, n_af)
+        CSV.write(joinpath(@__DIR__, "data/af.csv"), af)
+        config[:af_file] = abspath(@__DIR__, "data/af.csv")
+    end
+
+    if demand_shape_file
+        demand_shape = rand_demand_shape(;n_hours, n_demand_shape)
+        CSV.write(joinpath(@__DIR__, "data/demand_shape.csv"), demand_shape)
+        config[:demand_shape_file] = abspath(@__DIR__, "data/demand_shape.csv")
+    end
+    
+    return config
+end
+
+function years()
+    ["y$y" for y in 2030:5:2050]
+end
+function year_strs()
+    year_strs = vcat(years(), "")
+end
+
+function load_types()
+    ("ev", "residential", "commercial", "industrial", "transportation")
+end
+function countries()
+    ("narnia", "calormen", "archenland", "telmar")
+end
+
+
+function rand_demand(;n_bus, n_demand, kwargs...)
+    DataFrame(
+        "bus_idx" => rand(1:n_bus, n_demand),
+        "pd" => rand(n_demand),
+        "load_type" => rand(load_types())
+    )
+end
+
+function genfuels()
+    ("biomass","nuclear","ng","coal","solar","hydro", "wind", "geothermal")
+end
+gentypes() = OrderedDict(
+    "biomass"=>["biomass","biomass_new"],
+    "nuclear"=>["nuclear","nuclear_new"],
+    "ng"=>["ng","ng_cc","ng_ccccs"],
+    "coal"=>["coal","coal_ccs"],
+    "solar"=>["solar","solar_new"],
+    "hydro"=>["hydro","hydro_new"],
+    "wind"=>["wind","wind_new"],
+    "geothermal"=>["hydrothermal","deepgeo_new"],
+)
+
+function rand_af(;n_hours, n_af)
+    af = DataFrame(
+        "area" => String[],
+        "subarea" => String[],
+        "genfuel" => String[],
+        "gentype" => String[],
+        "year"=>String[],
+        "joint" => Int64[],
+        "status" => Bool[],
+        ("h$n"=>Float64[] for n in 1:n_hours)...
+    )
+    joint = 1
+    gt = gentypes()
+    yrs = year_strs()
+    while nrow(af) < n_af
+        for country in countries()
+            joint += 1
+            for genfuel in genfuels(), gentype in gt[genfuel]
+                year = rand(yrs)
+                push!(af, ("country", country, genfuel, gentype, year, joint, true, rand(n_hours)...))
                 if nrow(af) == n_af
                     break
                 end
             end
+            if nrow(af) == n_af
+                break
+            end
         end
-
-        CSV.write(joinpath(@__DIR__, "data/af.csv"), af)
-        config[:af_file] = abspath(@__DIR__, "data/af.csv")
-
     end
+    return af
+end
 
-    demand = DataFrame(
-        "bus_idx" => rand(1:n_bus, n_demand),
-        "pd" => rand(n_demand),
-        "load_type" => rand(["ev", "residential", "commercial", "industrial", "transportation"])
+function rand_demand_shape(;n_hours, n_demand_shape)
+    demand_shape = DataFrame(
+        "area" => String[],
+        "subarea" => String[],
+        "load_type" => String[],
+        "year"=>String[],
+        "joint" => Int64[],
+        "status" => Bool[],
+        ("h$n"=>Float64[] for n in 1:n_hours)...
     )
-    CSV.write(joinpath(@__DIR__, "data/demand.csv"), demand)
-    config[:demand_file] = abspath(@__DIR__, "data/demand.csv")
-    return config
+    joint = 1
+    gf = genfuels()
+    gt = gentypes()
+    yrs = year_strs()
+    lts = load_types()
+    while nrow(demand_shape) < n_demand_shape
+        for country in countries()
+            joint += 1
+            for lt in lts
+                year = rand(yrs)
+                push!(demand_shape, ("country", country, lt, year, joint, true, rand(n_hours)...))
+                if nrow(demand_shape) == n_demand_shape
+                    break
+                end
+            end
+            if nrow(demand_shape) == n_demand_shape
+                break
+            end
+        end
+    end
+    return demand_shape
 end
--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -16,7 +16,8 @@ SUITE["load_gen_table!"] = @benchmarkable E4ST.load_gen_table!(config, data) set
 SUITE["load_branch_table!"] = @benchmarkable E4ST.load_branch_table!(config, data) setup=(config=make_random_inputs(); data = load_data(config))
 SUITE["load_hours_table!"] = @benchmarkable E4ST.load_hours_table!(config, data) setup=(config=make_random_inputs(); data = load_data(config))
 SUITE["load_af_table!"] = @benchmarkable E4ST.load_af_table!(config, data) setup=(config=make_random_inputs(); data = load_data(config))
-SUITE["load_demand_table!"] = @benchmarkable E4ST.load_demand_table!(config, data) setup=(config=make_random_inputs(); data = load_data(config))
+SUITE["load_demand_table!"] = @benchmarkable E4ST.load_demand_table!(config, data) setup=(config=make_random_inputs(demand_shape_file=false); data = load_data(config))
+SUITE["load_demand_table! with shape"] = @benchmarkable E4ST.load_demand_table!(config, data) setup=(config=make_random_inputs(demand_shape_file=true); data = load_data(config))
 SUITE["get_generator"] = @benchmarkable get_generator(data, 1) setup=(config=make_random_inputs(); data = load_data(config)) evals=1000
 SUITE["get_af"] = @benchmarkable get_af(data, 50, 3, 50) setup=(config=make_random_inputs(); data = load_data(config)) evals=1000
 SUITE["get_pd"] = @benchmarkable get_pd(data, 50, 3, 50) setup=(config=make_random_inputs(); data = load_data(config)) evals=1000
