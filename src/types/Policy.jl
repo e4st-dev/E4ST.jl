4d47b7b0e101bef6721e1b0b9d7443d50a9c9aea
sallyrobson <srobson@RFF.org>
2023-03-01 17:29:10 -0500

    cleaning up and adding documentation

--- src/types/Policy.jl
+++ src/types/Policy.jl
@@ -6,29 +6,41 @@ This is a subtype of Modification that represents a policy to be modeled.
 abstract type Policy <: Modification end
 
 
+### Loading in Policies -----------------------------------
+
+
+
+
+### Basic Policy Types ------------------------------------
+
 """
     struct CES <: Policy
 
 Clean Energy Standard - Load serving entity must purchase a certain amount of clean energy credits. The number of credits for a type of generation depends on it's emissions relative to a benchmark.
 """
-#= struct CES <: Policy
-    name::
-    emiscol::Symbol 
+struct CES <: Policy
+    name::Symbol
+    value::OrderedDict #
     benchmark::Float64
-    cap_file::AbstractString 
 
-    function CES(;name,emiscol,benchmark,cap_file)
-        #create a container of cap value and years 
-        cap_table = load_table(joinpath(@__DIR__,cap_file))
-            #for each year in the sim that is in the cap_table, add it to a vector and then call set_yearly
-    end
+    # function CES(;name,emiscol,benchmark,cap_file)
+    #     #create a container of cap value and years 
+    #     cap_table = load_table(joinpath(@__DIR__,cap_file))
+    #         #for each year in the sim that is in the cap_table, add it to a vector and then call set_yearly
+    # end
 
-end =#
+end
 
 """
     struct PTC <: Policy
     
 Production Tax Credit - A \$/MWh tax incentive for the generation of specific technology or under specific conditions.
+
+name: policy name 
+value: \$/MWh value of the PTC, stored as an OrderedDict with years and the value (:y2020=>10), note year is a Symbol
+gen_age_min: minimum generator age to qualifying (inclusive)
+gen_age_max: maximum generator age to qualify (inclusive)
+gen_filters: filters for qualifying generators, stored as an OrderedDict with gen table columns and values (:emis_co2=>"<=0.1" for co2 emission rate less than or equal to 0.1)
 """
 Base.@kwdef struct PTC <: Policy
     name::Symbol
@@ -37,15 +49,14 @@ Base.@kwdef struct PTC <: Policy
     gen_age_max::Float64
     gen_filters::OrderedDict #Ethan adding a parse comparison that will work for ordered dicts 
 
-
-    # TODO: how to put a doc string here, or just document this function
-    # function PTC(value, start_year, end_year, gen_filters)
-    #     gen_filters = collect(gen_filters) #turn dictionary of filters into array of pairs
-    #     return new(Symbol(name), Float64(value), start_year, end_year, gen_filters)
-    # end
-
 end
 
+"""
+    function E4ST.modify_model!(pol::PTC, config, data, model)
+
+Creates a column in the gen table with the PTC value in each simulation year for the qualifying generators.
+Subtracts the PTC price * generation in that year from the objective function using `add_obj_term!(data, model, PerMWhGen(), pol.name, oper = -)`
+"""
 function E4ST.modify_model!(pol::PTC, config, data, model)
     
     gen = get_table(data, :gen)
@@ -71,20 +82,30 @@ end
 #TODO: something about how to process this in results
 #TODO: log statements
 
+"""
+    struct ITC <: Policy
 
+Investment Tax Credit - A tax incentive that is a percentage of capital cost given to generators that meet the qualifications. 
+
+name: policy name
+value: the credit level, stored as an OrderedDict with year and value (:y2020=>0.3)
+gen_filters: filters for qualifying generators, stored as an OrderedDict with gen table columns and values (:emis_co2=>"<=0.1" for co2 emission rate less than or equal to 0.1)
+"""
 Base.@kwdef struct ITC <: Policy
     name::Symbol
     value::OrderedDict
     gen_filters::OrderedDict
 
-    # function ITC(name, value, gen_age_min, gen_age_max, gen_filters)
-    #     gen_filters = collect(gen_filters) #turn dictionary of filters into array of pairs
-
-    #     #append!(gen_filters, [(:year_on=>(start_year,end_year))]) 
-    #     return new(Symbol(name), value, gen_age_min, gen_age_max, gen_filters)
-    # end
 end
 
+
+"""
+    function E4ST.modify_model!(pol::ITC, config, data, model)
+
+Creates a column in the gen table with the ITC value in each simulation year for the qualifying generators. 
+ITC values are calculated based on capex_obj so ITC values only apply in year_on for a generator.
+Subtracts the ITC price * Capacity in that year from the objective function using `add_obj_term!(data, model, PerMWCap(), pol.name, oper = -)`
+"""
 function E4ST.modify_model!(pol::ITC, config, data, model)
     gen = get_table(data, :gen)
     gen_idxs = get_row_idxs(gen, parse_comparisons(pol.gen_filters))
