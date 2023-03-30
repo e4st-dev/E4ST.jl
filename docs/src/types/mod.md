0d83ee8db0707ef80f5d5db62750a43c8ab70646
Ethan Russell <erussell@rff.org>
2023-03-30 17:33:20 -0400

    Add more documentation

--- docs/src/types/mod.md
+++ docs/src/types/mod.md
@@ -1,5 +1,6 @@
 Modifications
 =============
+
 ```@docs
 Modification
 modify_raw_data!(::Modification, config, data)
@@ -7,16 +8,4 @@ modify_setup_data!(::Modification, config, data)
 modify_model!(::Modification, config, data, model)
 modify_results!(::Modification, config, data)
 fieldnames_for_yaml(::Type{<:Modification})
-```
-
-## DCLine
-```@docs
-DCLine
-modify_raw_data!(mod::DCLine, config, data)
-modify_model!(mod::DCLine, config, data, model)
-```
-#### Column Summary for `dc_line` Table
-```@example
-using E4ST # hide
-summarize_table(:dc_line) # hide
 ```
--- src/types/modifications/CCUS.jl
+++ src/types/modifications/CCUS.jl
@@ -82,10 +82,10 @@ end
 
 Does the following:
 * Adds a column for carbon captured, `capt_co2`, based on `emis_co2`, and `capt_co2_percent`
-* reduces `emis_co2`by `capt_co2`
-* Splits up carbon capturing generators into 2
+* reduces `emis_co2` by `capt_co2`
+* Splits up carbon capturing generators into 2 separate generators - one for "saline" and one for "eor", and adjusts the eor emissions by `config[eor_leakage_rate]`
 * Add a column for `ccus_type` - either "eor", "saline", or "na"
-* Adds sets of indices to `data[:ccus_gen_sets]::Vector{Vector{Int64}}` 
+* Adds sets of indices to `data[:ccus_gen_sets]::Vector{Vector{Int64}}`
 """
 function modify_setup_data!(mod::CCUS, config, data)
     # TODO: think through how to un-group ccus matched generators, and how we would want to handle saving them and loading them in
