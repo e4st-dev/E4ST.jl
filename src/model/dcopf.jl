9f3b38402a05e55112a744d1f13e2c0dc7063883
Ethan Russell <erussell@rff.org>
2023-04-05 13:50:16 -0400

    Move type definitions up to the start of E4ST.jl to prevent conflicts

--- src/model/dcopf.jl
+++ src/model/dcopf.jl
@@ -286,23 +286,6 @@ function get_egen_gen(data, model, gen_idx, year_idx, hour_idx)
 end
 
 export get_egen_gen
-
-
-
-
-# Model Mutation Functions
-################################################################################
-"""
-    abstract type Term
-
-Abstract type Term is used to add variables (terms) to the objective function or other functions. Subtypes include PerMWhGen, PerMWCap, and PerMWhCurtailed. 
-"""        
-abstract type Term end
-
-struct PerMWhGen <: Term end
-struct PerMWCap <: Term end
-struct PerMWhCurtailed <: Term end
-
   
 
 """
