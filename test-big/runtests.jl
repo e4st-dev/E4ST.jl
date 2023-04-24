180d0317a7ec5c1964329db75039fde40917028c
Ethan Russell <erussell@rff.org>
2023-04-24 10:49:47 -0400

    Update to use new config

--- test-big/runtests.jl
+++ test-big/runtests.jl
@@ -10,9 +10,9 @@ using JuMP
             include("../test-gurobi/runtests.jl")
         end
         @testset "Full Tests" begin
-            config_2016 = "L:/Project-Gurobi/Workspace3/E4ST/Data/config/config_2016.yml"
+            config_2016 = "L:/Project-Gurobi/Workspace3/E4ST/erussell/repos/e4st-input-processing/data/config/config_2016.yml"
             config_gurobi = joinpath(@__DIR__, "config/config_gurobi.yml")
-            @time out_path, _ = run_e4st(config_2016, config_gurobi)
+            @time out_path, _ = run_e4st(config_2016, config_gurobi; build_gen_file = nothing)
 
             # @time config=read_config(config_file);
             # start_logging!(config)
