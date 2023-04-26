df59aabac266b1629f68116b76664f9530d6ca4d
Ethan Russell <erussell@rff.org>
2023-04-26 10:55:23 -0400

    Tweak gurobi testing slightly

---
+++ test-big/run-no-build.jl
@@ -0,0 +1,5 @@
+using Gurobi, E4ST
+config_2016 = "L:/Project-Gurobi/Workspace3/E4ST/erussell/repos/e4st-input-processing/data/config/config_2016.yml"
+config_gurobi = joinpath(@__DIR__, "config/config_gurobi.yml")
+base_out_path = joinpath(@__DIR__, "out/gurobi-no-expansion")
+@time out_path, _ = run_e4st(config_2016, config_gurobi; base_out_path, build_gen_file=nothing)
