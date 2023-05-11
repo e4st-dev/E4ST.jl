933d2a90beedebb024e58425ce9c8190ac688a23
sallyrobson <srobson@rff.org>
2023-05-11 17:14:52 -0400

    adding first_year_adj to PTC for result processing and other misc changes

--- src/types/policies/ITC.jl
+++ src/types/policies/ITC.jl
@@ -55,29 +55,6 @@ end
 Subtracts the ITC price * Capacity in that year from the objective function using [`add_obj_term!(data, model, PerMWCap(), pol.name, oper = -)`](@ref)
 """
 function E4ST.modify_model!(pol::ITC, config, data, model)
-    # gen = get_table(data, :gen)
-    # gen_idxs = get_row_idxs(gen, parse_comparisons(pol.gen_filters))
-
-    # @info "Applying ITC $(pol.name) to $(length(gen_idxs)) generators"
-
-    # years = get_years(data)
-
-    # #create column of annualized ITC values
-    # add_table_col!(data, :gen, pol.name, Container[ByNothing(0.0) for i in 1:nrow(gen)], DollarsPerMWBuiltCapacity,
-    #     "Investment tax credit value for $(pol.name)")
-
-    # #update column for gen_idx 
-    # #TODO: do we want the ITC value to apply to all years within econ life? Will get multiplied by capex_obj so will only be non zero for year_on but maybe for accounting? 
-    # credit_yearly = [get(pol.values, Symbol(year), 0.0) for year in years] #values for the years in the sim
-
-    # for gen_idx in gen_idxs
-    #     g = gen[gen_idx, :]
-
-    #     # credit yearly * capex_obj for that year, capex_obj is only non zero in year_on so ITC should only be non zero in year_on
-    #     vals_tmp = [credit_yearly[i]*g.capex_obj.v[i]  for i in 1:length(years)]
-    #     gen[gen_idx, pol.name] = ByYear(vals_tmp)
-    # end
-    # data[:gen] = gen
     add_obj_term!(data, model, PerMWCap(), pol.name, oper = -)
 end
 
--- src/types/policies/PTC.jl
+++ src/types/policies/PTC.jl
@@ -7,6 +7,7 @@ Production Tax Credit - A \$/MWh tax incentive for the generation of specific te
 
 * `name`: policy name 
 * `values`: \$/MWh values of the PTC, stored as an OrderedDict with years and the value `(:y2020=>10)`, note `year` is a `Symbol`
+* `first_year_adj`: If the PTC is for the first year that a generator is on, it is sometimes adjusted to be the average PTC value over the expected lifetime of the generator. This is the adjustement factor between the original and first year value `first_year_ptc = original_ptc*first_year_adj`
 * `gen_age_min`: minimum generator age to qualifying (inclusive)
 * `gen_age_max`: maximum generator age to qualify (inclusive)
 * `gen_filters`: filters for qualifying generators, stored as an OrderedDict with gen table columns and values (`:emis_co2=>"<=0.1"` for co2 emission rate less than or equal to 0.1)
@@ -14,6 +15,7 @@ Production Tax Credit - A \$/MWh tax incentive for the generation of specific te
 Base.@kwdef struct PTC <: Policy
     name::Symbol
     values::OrderedDict
+    first_year_adj::Float64 = 1
     gen_age_min::Float64
     gen_age_max::Float64
     gen_filters::OrderedDict
@@ -54,29 +56,23 @@ end
 Subtracts the PTC price * generation in that year from the objective function using [`add_obj_term!(data, model, PerMWhGen(), pol.name, oper = -)`](@ref)
 """
 function E4ST.modify_model!(pol::PTC, config, data, model)
-    
-    # gen = get_table(data, :gen)
-    # gen_idxs = get_row_idxs(gen, parse_comparisons(pol.gen_filters))
-
-    # @info "Applying PTC $(pol.name) to $(length(gen_idxs)) generators"
-
-    # years = get_years(data)
-
-    # #create column of PTC values
-    # add_table_col!(data, :gen, pol.name, Container[ByNothing(0.0) for i in 1:nrow(gen)], DollarsPerMWhGenerated,
-    #     "Production tax credit value for $(pol.name)")
-
-    # #update column for gen_idx 
-    # credit_yearly = [get(pol.values, Symbol(year), 0.0) for year in years] #values for the years in the sim
-    # for gen_idx in gen_idxs
-    #     g = gen[gen_idx, :]
-    #     g_qual_year_idxs = findall(age -> pol.gen_age_min <= age <= pol.gen_age_max, g.age.v)
-    #     vals_tmp = [(i in g_qual_year_idxs) ? credit_yearly[i] : 0.0  for i in 1:length(years)]
-    #     gen[gen_idx, pol.name] = ByYear(vals_tmp)
-    # end
-    # data[:gen] = gen
     add_obj_term!(data, model, PerMWhGen(), pol.name, oper = -)
 end
 
 
-#TODO: something about how to process this in results
+"""
+    E4ST.modify_results!(pol::PTC, config, data) -> 
+"""
+function E4ST.modify_results!(pol::PTC, config, data)
+    # Retrieve the table
+    results = get_results(data)
+
+    # create new columns with policy name and suffix
+
+    # adjust value of PTC by the first_year_adj
+
+
+
+
+    
+end
