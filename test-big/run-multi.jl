bce4642dd05e9c07bf8af26ea1b95abbf4118056
Ethan Russell <erussell@rff.org>
2023-05-16 10:58:33 -0400

    Remove test-big from repo

--- test-big/run-multi.jl
+++
@@ -1,5 +0,0 @@
-using Gurobi, E4ST
-config_2016 = "L:/Project-Gurobi/Workspace3/E4ST/erussell/repos/e4st-input-processing/data/config/config_2016.yml"
-config_gurobi = joinpath(@__DIR__, "config/config_gurobi.yml")
-base_out_path = joinpath(@__DIR__, "out/gurobi-multi")
-@time out_path, _ = run_e4st(config_2016, config_gurobi; base_out_path, years=["y2030", "y2035"])
