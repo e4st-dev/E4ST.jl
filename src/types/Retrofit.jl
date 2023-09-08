47e0e900c59eb536c415d6efabec940a2d76fdcd
Ethan Russell <erussell@rff.org>
2023-09-08 16:12:21 -0400

    Add more docs to retrofit function [skip ci]

--- src/types/Retrofit.jl
+++ src/types/Retrofit.jl
@@ -25,7 +25,13 @@ export can_retrofit
 """
     retrofit!(ret::Retrofit, newgen) -> ::AbstractDict
 
-Retrofits `newgen`, a `Dict` containing all the properties of the original generator, but with the `year_retrofit` already updated.  Note that the `capex` should be included in the retrofitted generator WITHOUT the existing generator's capex.  I.e. capex for the retrofit should be only the capital costs for the retrofit, not including the initial capital costs for building the generator.
+This function should change `newgen` to have the properties of the retrofit.  `newgen` is a `Dict` containing all the properties of the original generator, but with the the following fields already changed:
+* `year_retrofit` - set to the year to be retrofitted
+* `retrofit_original_gen_idx` - set to the index of the gen table for the original generator
+* `capex` - set to 0 to avoid double-paying capex for the already-built generator.  Capex added to `newgen` should only be the capital costs for the retrofit itself.  E4ST should already be accounting capex in `past_capex` for the original generator.
+* `pcap0` - set to 0
+* `transmission_capex` - set to 0
+* `build_status` - set to `unretrofitted`
 """
 function retrofit! end
 export retrofit!
