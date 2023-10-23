da43ffac799662ed36397d4fc2645e47728bbe3a
Ethan Russell <erussell@rff.org>
2023-10-23 12:01:31 -0400

    Document

--- src/results/formulas.jl
+++ src/results/formulas.jl
@@ -77,7 +77,7 @@ Arguments:
 * `table_name` - the name of the table that the result is calculated from, either directly or as a combination of other results
 * `result_name` - the name of the result being calculated.  Cannot be a column name within the table.
 * `formula` - `formula` can take two different forms.
-  * it can be a combination of columns to be aggregated directly from `table_name`.  I.e. `"SumHourly(vom, egen)"`. See [`Sum`](@ref), [`SumHourly`](@ref), [`SumYearly`](@ref), [`AverageYearly`](@ref), [`MinHourly`](@ref).
+  * it can be a combination of columns to be aggregated directly from `table_name`.  I.e. `"SumHourly(vom, egen)"`. See [`Sum`](@ref), [`SumHourly`](@ref), [`SumYearly`](@ref), [`AverageYearly`](@ref), [`MinHourly`](@ref).  Note that the columns here can either be column names or variable names stored with `data`, like `dam_co2`
   * it can also be a combination of other results. I.e. `"(vom_cost + fuel_cost) / egen_total"`.
 * `unit` - the [`Unit`](@ref) of the resulting number
 * `description` - a short description of the calculation.
