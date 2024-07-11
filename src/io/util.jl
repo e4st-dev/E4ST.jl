7ba6421b5093f8f04755315910b251802578f367
Ethan Russell <erussell@rff.org>
2024-07-11 10:30:05 -0400

    Fix issue for julia 1.9

--- src/io/util.jl
+++ src/io/util.jl
@@ -284,7 +284,7 @@ function comparison(value::AbstractString, ::Type{<:Union{Missing, <:Number}})
 end
 
 function comparison(value::AbstractString, ::Type{<:Union{Missing, <:Bool}})
-    num = parse(Bool, value)
+    num = parse(Bool, String(value))
     return ==(num)
 end
 function comparison(value::AbstractString, ::Type{<:Union{Missing, <:AbstractString}})
