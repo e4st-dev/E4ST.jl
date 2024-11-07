88fbdb4c295bc7e730ce8358670868a3010326b4
Ethan Russell <erussell@rff.org>
2024-11-06 20:54:12 -0500

    Add tests and corrections

--- src/types/modifications/CostAdder.jl
+++ src/types/modifications/CostAdder.jl
@@ -8,35 +8,35 @@ This adds to the cost of some subset of generators or storage facilities as seen
 * `cost_type::Symbol` - the type of cost to add to, either `variable` or `fixed`.
 * `table_name::Symbol` - the name of the table to add costs for, either `gen` or `storage`
 * `values::OrderedDict{Symbol}` - the cost to add, in DollarsPerMWh for `variable` costs, or DollarsPerMWCapacityPerHour for `fixed` costs.
-* `gen_filters::OrderedDict{Symbol}` - filters for qualifying generators, stored as an OrderedDict with gen table columns and values (`:emis_co2=>"<=0.1"` for co2 emission rate less than or equal to 0.1)
+* `filters::OrderedDict{Symbol} = OrderedDict()` - filters for qualifying generators / storage, stored as an OrderedDict with gen table columns and values (`:emis_co2=>"<=0.1"` for co2 emission rate less than or equal to 0.1).  Defaults to empty `OrderedDict`, affecting the whole table.
 """
 struct CostAdder <: Modification
     name::Symbol
     cost_type::Symbol
     table_name::Symbol
     values::OrderedDict{Symbol, Float64}
-    gen_filters::OrderedDict{Symbol}
+    filters::OrderedDict{Symbol}
 
     function CostAdder(;
             name,
             cost_type,
             table_name,
             values,
-            gen_filters
+            filters = OrderedDict{Symbol,Any}()
         )
         n = Symbol(name)
         ct = Symbol(cost_type)
         tn = Symbol(table_name)
         v = convert(OrderedDict{Symbol, Float64}, values)
-        if ct !in((:variable, :fixed))
+        if ct ∉ (:variable, :fixed)
             error("cost_type must be either variable or fixed for Modification CostAdder $name, but $ct given")
         end
 
-        if tn !in((:gen, :storage))
+        if tn ∉ (:gen, :storage)
             error("table_name must be either gen or storage for Modification CostAdder $name, but $tn given")
         end
 
-        return new(n,ct,tn, v, gen_filters)
+        return new(n,ct,tn, v, filters)
     end
 end
 export CostAdder
@@ -50,12 +50,12 @@ function E4ST.modify_setup_data!(m::CostAdder, config, data)
 
     table = get_table(data, m.table_name)
 
-    row_idxs = get_row_idxs(table, parse_comparisons(m.gen_filters))
+    row_idxs = get_row_idxs(table, parse_comparisons(m.filters))
 
     @info "Applying CostAdder $(m.name) to $(length(row_idxs)) rows of the $(m.table_name) table"
 
     years = get_years(data)
-    unit = m.cost_type == :variable ? DollarsPerMWh : DollarsPerMWCapacityPerHour
+    unit = m.cost_type == :variable ? DollarsPerMWhGenerated : DollarsPerMWCapacityPerHour
 
     costs = Container[ByNothing(0.0) for i in 1:nrow(table)]
     add_table_col!(data, m.table_name, m.name, costs,
@@ -63,17 +63,22 @@ function E4ST.modify_setup_data!(m::CostAdder, config, data)
         "The cost of CostAdder $(m.name) added to the objective, but not added to any results or welfare formulas"
     )
 
+    yrs_sym = Symbol.(years)
+
     for row_idx in row_idxs
-        cost = ByYear([get(m.values, yr, 0.0) for yr in years])
+        cost = ByYear([get(m.values, yr, 0.0) for yr in yrs_sym])
         costs[row_idx] = cost
     end
 end
 
 function E4ST.modify_model!(m::CostAdder, config, data, model)
     
-    make_cost_adder_exp!(m, config, data, model)        
+    make_cost_adder_exp!(m, data, model)   
+    
+    term = m.cost_type == :variable ? PerMWhGen() : PerMWCap()
 
-        
+
+    add_obj_exp!(data, model, term, m.name; oper= (+))
         
 end
 
@@ -83,7 +88,7 @@ function make_cost_adder_exp!(m, ::Val{:variable}, ::Val{:gen}, table, data, mod
     nyr = get_num_years(data)
     nhr = get_num_hours(data)
     hour_weights = get_hour_weights(data)
-    col = table[!, m.name]::Vector{Container}
+    col = table[!, m.name]::Vector{<:Container}
 
     var = model[:pgen_gen]::Array{VariableRef, 3}
     model[m.name] = @expression(model,
@@ -98,7 +103,7 @@ function make_cost_adder_exp!(m, ::Val{:fixed}, ::Val{:gen}, table, data, model)
     nyr = get_num_years(data)
     nhr = get_num_hours(data)
     hours_per_year = sum(get_hour_weights(data))
-    col = table[!, m.name]::Vector{Container}
+    col = table[!, m.name]::Vector{<:Container}
 
     var = model[:pcap_gen]::Array{VariableRef, 2}
     model[m.name] = @expression(model,
@@ -111,7 +116,7 @@ function make_cost_adder_exp!(m, ::Val{:variable}, ::Val{:storage}, table, data,
     nyr = get_num_years(data)
     nhr = get_num_hours(data)
     hour_weights = get_hour_weights(data)
-    col = table[!, m.name]::Vector{Container}
+    col = table[!, m.name]::Vector{<:Container}
 
     var = model[:pdischarge_stor]::Array{VariableRef, 3}
     model[m.name] = @expression(model,
@@ -125,7 +130,7 @@ function make_cost_adder_exp!(m, ::Val{:fixed}, ::Val{:storage}, table, data, mo
     nyr = get_num_years(data)
     nhr = get_num_hours(data)
     hours_per_year = sum(get_hour_weights(data))
-    col = table[!, m.name]::Vector{Container}
+    col = table[!, m.name]::Vector{<:Container}
 
     var = model[:pcap_stor]::Array{VariableRef, 2}
     model[m.name] = @expression(model,
