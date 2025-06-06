6036a6aa48f3c130c276e5bd4cbba62a47d9f1bf
McKenna Peplinski <56940653+mckennapep@users.noreply.github.com>
2025-06-05 22:09:16 -0400

    Update src/results/parse.jl
    Co-authored-by: Ethan Russell <44298283+Ethan-Russell@users.noreply.github.com>

--- src/results/parse.jl
+++ src/results/parse.jl
@@ -1,7 +1,7 @@
 """
     parse_results!(config, data, model) -> nothing
     
-* Gathers the values and shadow prices of each variable, expression, and constraint stored in the model, unscales the shadow pirces, and dumps them into `data[:results][:raw]` (see [`get_raw_results`](@ref) and [`get_results`](@ref)).
+* Gathers the values and shadow prices of each variable, expression, and constraint stored in the model, unscales the shadow prices, and dumps them into `data[:results][:raw]` (see [`get_raw_results`](@ref) and [`get_results`](@ref)).
 * Adds relevant info to `gen`, `bus`, and `branch` tables.  See [`parse_lmp_results!`](@ref) and [`parse_power_results!`](@ref) for more information.
 * Saves updated `gen` table via [`save_updated_gen_table`](@ref)
 * Saves `data` to `get_out_path(config,"data_parsed.jls")` unless `config[:save_data_parsed]` is `false` (true by default).
