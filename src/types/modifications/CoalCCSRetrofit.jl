05b84a4ef75a50ebc385464f718572117c2e1548
Peplinski <mpeplinski@rff.org>
2025-10-02 13:15:53 -0400

    Tweaks to retrofits

--- src/types/modifications/CoalCCSRetrofit.jl
+++ src/types/modifications/CoalCCSRetrofit.jl
@@ -38,6 +38,10 @@ function init!(ret::CoalCCSRetrofit, config, data)
     if !hasproperty(gen, :pcap_plant_avg)
         @warn "gen table does not have column pcap_plant_avg, representing the average nameplate plant capacity of a generator, in MW.  \nAssuming that the average plant capacity is the same as the max capacity."
         add_table_col!(data, :gen, :pcap_plant_avg, copy(gen.pcap_max), MWCapacity, "Average MW capacity of each plant in a representative generator.")
+    elseif any(x -> x == -Inf, gen.pcap_plant_avg)
+        @warn "gen table has some default pcap_plant_avg values (-inf), representing the average nameplate plant capacity of a generator, in MW.  \nFor these values, assuming that the average plant capacity is the same as the max capacity."
+        println("there are some-inf values")
+        gen.pcap_plant_avg = ifelse.(gen.pcap_plant_avg .== -Inf, gen.pcap_max, gen.pcap_plant_avg)  
     end
     return
 end
@@ -62,7 +66,7 @@ function retrofit!(ret::CoalCCSRetrofit, newgen)
     if pcap_pen < 0
         @warn "Pcap penalty is less than zero for CoalCCSRetrofit with heat_rate = $hr, and pcap_plant_avg=$pcap_avg"
     end
-
+    
     # Adjust the costs.  These costs come from a regression in EPA
     # Schedule 6 data. For more information, see the folder:
     # L:\Project-Gurobi\Workspace3\E4ST_InputDev\02_gen\03_NewGenerators\CCS\EPA
@@ -79,6 +83,12 @@ function retrofit!(ret::CoalCCSRetrofit, newgen)
     haskey(newgen, :emis_so2)  && (newgen[:emis_so2]  *= ((1 - ret.reduce_so2_percent)  * (1 + hr_pen)))
     haskey(newgen, :emis_pm25) && (newgen[:emis_pm25] *= ((1 - ret.reduce_pm25_percent) * (1 + hr_pen)))
     newgen[:pcap_max] *= (1 - pcap_pen)
+    if newgen[:pcap_max] <0 
+        println("negative pcapmax")
+        println(pcap_pen)
+        println(pcap_avg)
+        println(hr)
+    end
 
     newgen[:gentype] = "coal_ccus_retrofit"
 
